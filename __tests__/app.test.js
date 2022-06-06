97e4797b6e98b4497f77b8461f63b8621fc0d4de
Cole Rossman <colerossman99@gmail.com>
2022-06-06 15:57:17 -0700

    routes changed in signs.js and test written for /zodiac to see if it returns a list of signs with id and name

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,19 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('week19-zodiacapi-express-scratch routes', () => {
+const { signs } = require('../data/signs');
+
+describe('zodiac sign routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('/zodiac should return a list of zodiac signs with id and name', async () => {
+    const res = await request(app).get('/zodiac');
+    const expected = signs.map((sign) => {
+      return { id: sign.id, name: sign.name };
+    });
+    expect(res.body).toEqual(expected);
   });
 
   afterAll(() => {
--- data/signs.js
+++ data/signs.js
@@ -72,3 +72,5 @@ const signs = [
     symbol: "Virgin"
     }
     ]
+
+    module.exports = { signs };
